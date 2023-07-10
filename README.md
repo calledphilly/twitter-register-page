fc02b9393febdb3c504419764be5afd024102fe6
calledphilly <134594230+calledphilly@users.noreply.github.com>
2023-07-10 17:34:11 +0200

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# twitter register page
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html'
+  ],
+  theme: {
+    colors: {
+      'white': '#ffffff',
+      'twitter-white': '#E6E9EA',
+      'black': '#000000',
+      'bleu': '#242D35',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
